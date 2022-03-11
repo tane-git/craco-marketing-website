1a63e6c3365c72a5dbbc1e6b8709318cfd7ead83
tane-git <tane.alexander.wilson@gmail.com>
2022-03-11 20:35:08 +1300

    investigate different ways to apply background

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
 import { Layout, Tag } from 'antd';
 import React from 'react';
+import { Background } from './components/Background';
 import './styles/index.less';
 
 function App() {
   return (
     <>
-      <Layout className='background'>
+      {/* <Layout className='background'> */}
         {/* <img src='/background/set1.svg' alt='background' /> */}
         {/* <Header /> */}
         {/* <Content /> */}
         {/* <Footer /> */}
-      </Layout>
+        {/* <div className='background'> */}
+          {/* <Background /> */}
+        {/* </div> */}
+      {/* </Layout> */}
+
+      {/* APPLYING SVG'S WITH WHITE BACKGROUND */}
+      {/* <div className='background'>
+        <div className='background1'></div>
+        <div className='background2'></div>
+      </div> */}
+
+      {/* TRYING TRANSPARENT BACKGROUND */}
+      {/* <div className='transparentBackground'></div> */}
+
+      {/* TRYING TO IMPLEMENT BACKGROUND WITH CSS */}
+      {/* OR Individual SVGS */}
+      <Background />
+
+      {/* TRYING TO IMPLEMENT BACKGROUND WITH DIFFERENT SVG EXPORTS FROM FIGMA */}
+      {/* <div className='background'></div> */}
     </>
   );
 }
--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -3,8 +3,19 @@ import styles from './styles/style.module.less'
 
 export const Background: React.FC = () => {
   return (
-    <>
-      <div className={styles.polygon4}></div>
-    </>
+    // <div className={styles.background}>
+    //   <div className={styles.ellipse1}></div>  
+    //    {/* <div className={styles.ellipse18}></div>   */}
+    //    {/* <div className={styles.polygon2}></div> */}
+    //    {/* <div className={styles.polygon3}></div> */}
+    //    {/* <div className={styles.polygon4}></div> */}
+    // </div>
+
+    // // individual svgs
+    <div>
+       {/* <img src='./images/Ellipse1.svg' alt='ellipse1' /> */}
+       {/* <img src='/svgs/Ellipse1.svg' alt='ellipse1' /> */}
+       <img src='/pngs/Ellipse1.png' alt='ellipse1' />
+    </div>
   )
 }
