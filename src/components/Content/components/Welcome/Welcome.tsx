7e1995f879014de0f5750cc6deeb9cc28358cf4f
tane-git <tane.alexander.wilson@gmail.com>
2022-03-13 19:10:45 +1300

    trying to fix theme.less import errors

--- src/components/Content/components/Welcome/Welcome.tsx
+++ src/components/Content/components/Welcome/Welcome.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './style/style.module.less';
+import Robots from './images/subQueryRobots.svg';
 
 export const Welcome: React.FC = () => {
   return (
-    <div className={styles.welcome}>
-      Query Decentralised Data <span className={styles.gradient}>Faster</span> 
-    </div>
+    <>
+      <div className={styles.welcome}>
+        Query Decentralised Data <span className={styles.gradient}>Faster</span> 
+      </div>
+      <div className={styles.robots}>
+        {/* <img src={Robots} alt='Diagram of how SubQuery works, using a robot analogy' /> */}
+      </div>
+    </>
   )
 }
