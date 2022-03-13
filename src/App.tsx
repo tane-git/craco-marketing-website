6332a7b51cc852853a9a6c0542c0b05547068bb1
tane-git <tane.alexander.wilson@gmail.com>
2022-03-13 15:57:03 +1300

    more tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Layout, Tag } from 'antd';
+import { Layout } from 'antd';
 import React from 'react';
-import { Background } from './components';
+
+import {
+  Background,
+  Header
+} from './components';
 import './styles/index.less';
 
 function App() {
   return (
     <>
       <Background />
+      <Layout>
+        <Header />
+      </Layout>
     </>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from 'antd';
+
+export const Header: React.FC = () => {
+  return (
+    <Layout.Header>
+      SubQuery
+    </Layout.Header>
+  )
+}
